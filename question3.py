120979ea200b7c265a64ffaa231218e3fa481fee
Ranjana Rauniyar <ranjanarauniyar384@gmail.com>
2026-02-12 19:27:15 +0530

    find the greatest of 3 numbers entered by the user

---
+++ question3.py
@@ -0,0 +1,11 @@
+#WAP to find the greatest of 3 numbers entered by the user.
+a = int(input("enter first number: "))
+b = int(input("enter second number: "))
+c = int(input("enter third number: "))
+
+if(a >= b and a >= c):
+    print("first number is largest", a)
+elif(b >= c):
+    print("secont number is largest", b)
+else:
+    print("thirsd is ;largest", c)
