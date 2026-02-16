372a0c037120188d1efd7f21e5f507aacd5aadca
Ranjana Rauniyar <ranjanarauniyar384@gmail.com>
2026-02-16 23:19:57 +0530

    list ko discending order me print

---
+++ discendorder.py
@@ -0,0 +1,5 @@
+list = ["banana", "apple", "cherry", "date"]
+print(list.sort())
+print(list)
+
+
--- le3multipledata.py
+++ le3multipledata.py
@@ -1,2 +1,4 @@
 student = ["Ranjana", 95.4, 20, "Delhi"]
+print(student[0])
+student[0] = "Ranjana"
 print(student)
