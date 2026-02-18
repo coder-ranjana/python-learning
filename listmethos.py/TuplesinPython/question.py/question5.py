f92072959428992d7403d013077297a78b44b6b5
Ranjana Rauniyar <ranjanarauniyar384@gmail.com>
2026-02-18 19:51:14 +0530

    WAP to ask the user to enter names of their 5 favorit movies & storie them in a list

---
+++ listmethos.py/TuplesinPython/question.py/question5.py
@@ -0,0 +1,12 @@
+# WAP to ask the user to enter names of their 5 favorit movies & storie them in a list
+
+movies = []
+mov = input("enter 1st movie: ")
+movies.append(mov)
+mov = input("enter 2nd movie: ")
+movies.append(mov)
+mov = input("enter 3rd movie: ")
+movies.append(mov)
+
+
+print(movies)
