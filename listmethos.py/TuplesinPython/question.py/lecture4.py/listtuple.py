edd13e5573cbf053e22b08fcdf5ef738170caddf
Ranjana Rauniyar <ranjanarauniyar384@gmail.com>
2026-02-20 22:41:16 +0530

    list and tuple store

---
+++ listmethos.py/TuplesinPython/question.py/lecture4.py/listtuple.py
@@ -0,0 +1,10 @@
+info = {
+    "key" : "value",
+    "subject" : ["python", "c", "java", "php"],
+    "topics" : ("dict", "set"),
+    "age" : 20,
+    "is_student" : True,
+    "marks" : 95.5
+ }
+
+print(info)
