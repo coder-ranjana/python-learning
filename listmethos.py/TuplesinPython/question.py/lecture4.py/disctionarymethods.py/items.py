67562ad08eb2722e1439ac37a54e9aaddca262ca
Ranjana Rauniyar <ranjanarauniyar384@gmail.com>
2026-02-23 19:32:03 +0530

    returns all (key, val) pairs as tuples

---
+++ listmethos.py/TuplesinPython/question.py/lecture4.py/disctionarymethods.py/items.py
@@ -0,0 +1,11 @@
+# returns all (key, val) pairs as tuples
+student = {
+  "name" : "Ranjana Rauniayr",
+  "subject" : {
+   "math" : 80,
+   "chem" : 90,
+   "phy" : 75,
+   "engl" :95
+  }
+ }
+print(list(student.items()))
