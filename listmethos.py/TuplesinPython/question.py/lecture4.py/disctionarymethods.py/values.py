ec5e2c40e3028f496f6f8e24898e11699158aa23
Ranjana Rauniyar <ranjanarauniyar384@gmail.com>
2026-02-23 19:03:52 +0530

    returns all values

---
+++ listmethos.py/TuplesinPython/question.py/lecture4.py/disctionarymethods.py/values.py
@@ -0,0 +1,11 @@
+student = {
+    "name" : "Ranjana Rauniyar",
+    "subject" : {
+        "Math" : 80,
+        "chem" : 75,
+        "phy" : 85,
+        "engl" :90
+    }
+}
+
+print(list(student.values()))
