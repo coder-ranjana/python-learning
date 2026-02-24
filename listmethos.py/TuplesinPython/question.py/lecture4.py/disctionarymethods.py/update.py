a7867d9506327091e9b6528b59a08cb2c1203464
Ranjana Rauniyar <ranjanarauniyar384@gmail.com>
2026-02-24 23:28:51 +0530

    insert the specified items to the dictionary

---
+++ listmethos.py/TuplesinPython/question.py/lecture4.py/disctionarymethods.py/update.py
@@ -0,0 +1,12 @@
+student = {
+    "name" : "Ranjana Rauniyar",
+    "subject" : {
+        "math" : 79,
+        "chem" :80,
+        "phy" :80
+    }
+}
+
+student.update({"city" : "delhi"})
+
+print(student)
