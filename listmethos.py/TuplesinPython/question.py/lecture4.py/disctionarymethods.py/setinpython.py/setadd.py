8b001c268097d93d1fff465c12247ed606596e5f
Ranjana Rauniyar <ranjanarauniyar384@gmail.com>
2026-02-27 22:21:37 +0530

    duplicate value  will not be added to the set

--- listmethos.py/TuplesinPython/question.py/lecture4.py/disctionarymethods.py/setinpython.py/setadd.py
+++ listmethos.py/TuplesinPython/question.py/lecture4.py/disctionarymethods.py/setinpython.py/setadd.py
@@ -3,6 +3,8 @@ collection.add(1)
 collection.add(2)
 collection.add(2)
 collection.add("Ranjana Rauniayr")
+collection.add((1, 2, 3))
+collection.add((1, 2, 3))# duplicate value will not be added to the set
 print(collection)
 
 
