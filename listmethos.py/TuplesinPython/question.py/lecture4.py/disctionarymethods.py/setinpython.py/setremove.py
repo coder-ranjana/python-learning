ff938799669d866f802e119c2a844c38e2db24aa
Ranjana Rauniyar <ranjanarauniyar384@gmail.com>
2026-02-27 22:33:05 +0530

    this will raise an error becouse the element is not persent in the set)

--- listmethos.py/TuplesinPython/question.py/lecture4.py/disctionarymethods.py/setinpython.py/setremove.py
+++ listmethos.py/TuplesinPython/question.py/lecture4.py/disctionarymethods.py/setinpython.py/setremove.py
@@ -6,5 +6,5 @@ collection.add("Ranjana Rauniyar")
 collection.add((1, 2, 3))
 
 
-
+collection.remove("Ranjana Rauniyar")#this will raise an error becouse the element is not persent in the set)
 print(len(collection))
