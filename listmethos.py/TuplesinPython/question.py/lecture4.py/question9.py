642ea6567914357e5d4faf87de30c371f58a83e0
Ranjana Rauniyar <ranjanarauniyar384@gmail.com>
2026-03-11 20:48:46 +0530

    list of subjects  for students, Assume one classroom is required for 1 subject

---
+++ listmethos.py/TuplesinPython/question.py/lecture4.py/question9.py
@@ -0,0 +1,11 @@
+#you are given a list of subjects  for students, Assume one classroom is required for 1 subject. How many classroom are needed by all students.
+subject = {
+    "python", "java", "c++", "python", "javascript", "java",
+    "python", "java", "c++", "c"
+
+}
+
+
+print(subject)
+
+print(len(subject))
