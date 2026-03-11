7209513703d0d65b2bd2a25844544eb20054f57a
Ranjana Rauniyar <ranjanarauniyar384@gmail.com>
2026-03-11 19:30:54 +0530

    Store  word meanings in a python dictionary

--- listmethos.py/TuplesinPython/question.py/lecture4.py/question8.py
+++ listmethos.py/TuplesinPython/question.py/lecture4.py/question8.py
@@ -1,10 +1,10 @@
 #Store following word meanings in a python dictionary:
 #table: "a piece of furniture", "list of facts & figures"
 #cat : "a small animal"
-dictionary = {
-    "cat" : "asmall animal",
-    "table" : ["a piece of furniture", "list of fscts &figures"]
 
+dictionary = {
+    "cat" : "a small animal",
+    "table" : ["a piece of furniture", "list of facts & figures"]
 }
 
 print(dictionary)
