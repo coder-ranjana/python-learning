8f604090bff5b4cbc10b21d3d4ab0a0fc7fe4079
Ranjana Rauniyar <ranjanarauniyar384@gmail.com>
2026-03-12 11:25:52 +0530

    separate value in the set

--- listmethos.py/TuplesinPython/question.py/question11.py
+++ listmethos.py/TuplesinPython/question.py/question11.py
@@ -1,3 +1,10 @@
 #Figure out a way to store 9 & 9.0 as separate value in the set.(you can take help of built-in date types)
 values = {9, 9.0}
 print(values)
+
+values = {
+    ("float", 9.0),
+    ("int", 9)
+
+}
+print(values)
