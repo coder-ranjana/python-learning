f58490569c52587734d6346111836c3d62578417
Ranjana Rauniyar <ranjanarauniyar384@gmail.com>
2026-03-14 18:43:46 +0530

    print element of following list using a loop

---
+++ listmethos.py/TuplesinPython/question.py/chapter5.py/whileloopquestion.py/question4.py
@@ -0,0 +1,9 @@
+# print the elements of  the following list using a loop:
+nums = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+heroes = ["ironman", "thor", "superman", "atman"]
+
+idx = 0
+while idx < len(heroes):
+    print(heroes[idx])
+    idx += 1
+    
