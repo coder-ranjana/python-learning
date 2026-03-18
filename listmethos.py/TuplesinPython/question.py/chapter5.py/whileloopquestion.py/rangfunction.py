3047c3bc8f96cc17cdb0b895b20801c42cc34c33
Ranjana Rauniyar <ranjanarauniyar384@gmail.com>
2026-03-18 11:42:47 +0530

    range type print

--- listmethos.py/TuplesinPython/question.py/chapter5.py/whileloopquestion.py/rangfunction.py
+++ listmethos.py/TuplesinPython/question.py/chapter5.py/whileloopquestion.py/rangfunction.py
@@ -1,6 +1,11 @@
-seq = range(5)
-print(seq[0])
-print(seq[1])
-print(seq[2])
-print(seq[3])
-print(seq[4])
+# Range functions returns a sequence of numbers, starting from 0 by  defalt, and increment by 1(by default), and stops before a specified number
+
+seq = range(10)
+#print(seq[0])
+#print(seq[1])
+#print(seq[2])
+#print(seq[3])
+#print(seq[4]) ,# loop lgana
+
+for i in seq:
+    print(i)
